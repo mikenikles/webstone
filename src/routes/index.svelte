ade9c224d39b8312fb99ecf0017a6450dbedd495
Mike Nikles <mikenikles@gmail.com>
2021-03-06 22:25:04 +0000

    Replace the Sapper boilerplate with a temporary Webstone landing page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,21 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+	import Quote from "../components/quote.svelte";
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
 	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
+		text-align: center;
 	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<style>
+		body {
+			color: #404040;
+			background: #dce1e1;
+		}
+	</style>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<Quote />
+<p>Follow me on Twitter <a href="https://twitter.com/mikenikles">@mikenikles</a> for updates on this project.</p>
